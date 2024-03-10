ff6fe760f6cb89460aab35cb2d401fe9343199fc
zzx <zzx@bigtree.xyz>
2024-03-11 03:42:24 +0800

    add code of download light curves from lightkurve

--- DownloadLC/main.py
+++ DownloadLC/main.py
@@ -1,11 +1,35 @@
 import sys
 import lightkurve as lk
+from astropy.coordinates import SkyCoord
+from . import tess_data
+import argparse
+
+
+def get_objname(ra, dec):
+    coo = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
+    return 'J' + coo.to_string(style='hmsdms', sep='', precision=1)
 
 
 def main():
-    ra = float(sys.argv[1])
-    dec = float(sys.argv[2])
+    parser = argparse.ArgumentParser(description='Download light curves')
+    parser.add_argument('ra', type=float, help='RA of the target')
+    parser.add_argument('dec', type=float, help='Dec of the target')
+    parser.add_argument('--output', type=str, help='Output object name', default=None)
+    parser.add_argument('--format', type=str, help='Output file format', default='csv', choices=['csv', 'fits'])
+    parser.add_argument('--survey', type=list, help='Survey to download from', 
+        default=['TESS', 'Kepler', 'K2', 'CoRoT', 'SPOC', 'EVEREST', 'K2SFF', 'K2SC', 'K2VARCAT', 'K2CTL', 'K2CFL', 'ZTF', 'ASAS-SN'])
+    parser.add_argument('--max_retries', type=int, help='Max retries for downloading', default=5)
+    args = parser.parse_args()
+    ra = args.ra
+    dec = args.dec
+    objname = args.output
+    if objname is None:
+        objname = get_objname(ra, dec)
+    outformat = args.format
+    surveys = args.survey
+    max_retries = args.max_retries
     print('Downloading data for RA:', ra, 'Dec:', dec)
+    tess_data.download_lightkurve_lc(ra, dec, objname, outformat, max_retries)
     # lc = lk.search_lightcurve(ra, dec).download_all()
 
 
---
+++ DownloadLC/tess_data.py
@@ -0,0 +1,58 @@
+import lightkurve as lk
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+
+def search_lc(coo, max_retries=5):
+    for ind in range(1, max_retries+1):
+        try:
+            search_res = lk.search_lightcurve(coo, radius=8*u.arcsec)
+            return search_res
+        except Exception as e:
+            print(f'Error: {e}')
+            print(f'Retrying for {ind} time, for {coo}, max retries {max_retries}')
+            continue
+    return None
+
+
+def download_lc(res, max_retries=5):
+    for ind in range(1, max_retries+1):
+        try:
+            lc = res.download()
+            return lc
+        except Exception as e:
+            print(f'Error: {e}')
+            print(f'Retrying for {ind} time, for {res}, max retries {max_retries}')
+            continue
+    return None
+
+
+def download_lightkurve_lc(ra, dec, objname, outformat, max_retries=5):
+    coo = SkyCoord(ra=ra, dec=dec, unit=(u.deg, u.deg))
+    results = search_lc(coo, max_retries)
+
+    results = lk.search_lightcurve(coo, radius=0.2*u.deg)
+    
+    if results is None:
+        raise ValueError('No light curve found')
+
+    for result in results:
+        mission = result.mission[0]
+        author = result.author.data[0]
+        dist = result.distance.value[0]
+        tmission = mission.replace(' ', '-')
+        outname = f'{objname}_{tmission}_{author}_dist{dist}.{outformat}'
+        lc = download_lc(result, max_retries)
+        if lc is None:
+            print(f'Error in downloading {result}')
+            continue
+        if outformat == 'csv':
+            try:
+                lc.to_fits(outname, overwrite=True)
+            except Exception as e:
+                print(f'Error in writing {outname} to fits')
+                print(f'Error: {e}')
+                outname = outname.replace('.fits', '.csv')
+                lc.to_csv(outname, overwrite=True)
+        else:
+            lc.to_csv(outname, overwrite=True)
